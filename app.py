7fecc25a06fd16398de48acd247a41d82ab588ef
SilviAdinata <silvima@mtechpro.com>
2026-05-04 16:01:23 +0700

    rtest2

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ print("this is for netskope test")
 print("this is for netskope test")
 #https://github.com/mtid-netskopelab/mtid-netskopetest.git
 #testestest
-#hellooooo
+#hellooooo#ttdtd
 #dfsdkjfdjks
